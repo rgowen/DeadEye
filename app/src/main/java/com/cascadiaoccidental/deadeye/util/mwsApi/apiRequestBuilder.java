c38dadf56cd98a1cb36ee902ed7e5f881a80b2ef
rgowen <ryan.gowen@gmail.com>
2017-12-18 23:11:55 -0800

    Update apiRequestBuilder.java

--- app/src/main/java/com/cascadiaoccidental/deadeye/util/mwsApi/apiRequestBuilder.java
+++ app/src/main/java/com/cascadiaoccidental/deadeye/util/mwsApi/apiRequestBuilder.java
@@ -4,11 +4,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
 
 public class ApiRequestBuilder  {
-    private final String SELLER_ID = "A10B1Y5YAX9LJB";
-    private final String MARKETPLACE_ID = "ATVPDKIKX0DER";
-    private final String AWS_ACCESS_KEY_ID = "AKIAIBM7Q3KK76F4EAMA";
-    private final String SECRET_KEY = "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";
-    private final String ASIN;
+
     public ApiRequestBuilder(String asin) {
         this.ASIN = asin;
     }
